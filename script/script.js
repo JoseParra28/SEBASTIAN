200c9b0b518e62f5ab8b1ec65e1060c33411d9f4
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-11-20 20:32:05 +0000

    js scroll
    added a parallax style scroll event

--- script/script.js
+++ script/script.js
@@ -1,2 +1,15 @@
 const scrollOne = document.querySelector(".hero-img");
 
+window.addEventListener("scroll", () => {
+
+    const scrolled = window.scrollY
+    console.log(scrolled)
+
+    if (scrolled >= 400 ){
+        document.querySelector(".hero-img").style.position = "relative";
+        document.querySelector(".hero-img").style.width = "70%";
+        document.querySelector(".hero-img").style.margin = "75%";
+    } else{
+        console.log("It works")
+    }
+})
