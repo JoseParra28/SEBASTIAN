bb3f489eb0567a81ca15a59b6085dba004038ff5
Jose Parra <110458931+JoseParra28@users.noreply.github.com>
2024-11-27 19:53:32 +0000

    popup
    adding functionality

--- script/gallery.js
+++ script/gallery.js
@@ -5,5 +5,28 @@ const galleryImg = document.querySelectorAll(".img")
 const closePopUp = document.querySelector(".close-popup")
 const popUp = document.querySelector(".popup-img")
 
-console.log(galleryImg)
 
+
+for (i = 0; i < galleryImg.length; i++){
+    galleryImg[i].addEventListener("click", function(){
+        let changeImg = this.alt
+        switch (changeImg) {
+            case "img-1":
+                let image = document.querySelectorAll(".img")
+                image.src = '/assets/image-asset (5).jpeg'
+                popUp.style.background = '/assets/image-asset (5).jpeg'
+                popUp.classList.remove("hidden")
+                console.log(this)
+                break;
+        
+            default:
+                break;
+        }
+        
+    })
+}
+   
+
+closePopUp.addEventListener("click", function(){
+    popUp.classList.add("hidden")
+})
