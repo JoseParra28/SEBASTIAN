ec997fde604b5a8d6d0af8c6a9cfe2a3b3ef99a8
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-02 16:02:33 +0000

    sc
    adding logic to phase 3

--- script/schedule.js
+++ script/schedule.js
@@ -9,18 +9,26 @@ for(let i = 0; i < scBtnContent.length; i++){
         
                 let phaseOne = document.querySelector(".phase-1");
                 let phaseTwo = document.querySelector(".phase-2");
-                let phaseThree = document.querySelector(".phase-2");
-                let phaseFour = document.querySelector(".phase-2");
+                let phaseThree = document.querySelector(".phase-3");
+                let phaseFour = document.querySelector(".phase-4");
 
 
                 let trackerOne = document.querySelector(".tracker-1 ")
                 let trackerTwo = document.querySelector(".tracker-2 ")
+                let trackerThree = document.querySelector(".tracker-3 ")
+                let trackerFour = document.querySelector(".tracker-4 ")
 
                 phaseOne.classList.add("hidden")
                 phaseTwo.classList.remove("hidden")
 
                 trackerOne.classList.remove("current")
                 trackerTwo.classList.add("current")
+
+                // phaseTwo.classList.add("hidden")
+                // phaseThree.classList.remove("hidden")
+
+                // trackerOne.classList.remove("current")
+                // trackerTwo.classList.add("current")
 
                 submitBtn.addEventListener("click", function (){
 
@@ -32,6 +40,9 @@ for(let i = 0; i < scBtnContent.length; i++){
                     let surnameDisplay = document.querySelector(".cust-name")
                     // let nameDisplay = document.querySelector(".cust-name")
                     // let nameDisplay = document.querySelector(".cust-name")
+
+                    phaseTwo.classList.add("hidden")
+                    phaseThree.classList.remove("hidden")
 
                     console.log(`you have selected ${currentBtn} `)
                     console.log(getDate)
