817298bc25039d1ed2446e49dbe2f96d0b89f09e
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-02 16:31:58 +0000

    sc
    finishing phase 3

--- script/schedule.js
+++ script/schedule.js
@@ -34,12 +34,6 @@ for(let i = 0; i < scBtnContent.length; i++){
                     let custInfo = document.querySelector(".cust-info");
                     let getDate = document.querySelector(".date-field").value;
                     let getTime = document.querySelector(".time-field").value;
-                    let getName = document.querySelector(".name-field").value;
-
-                    let nameDisplay = document.querySelector(".name-field").value;
-                    let surnameDisplay = document.querySelector(".surname-field").value;
-                    let nameEmail = document.querySelector(".email-name").value;
-                    let returnDisplay = document.querySelector(".returning").value;
 
                     phaseTwo.classList.add("hidden")
                     phaseThree.classList.remove("hidden")
@@ -48,13 +42,24 @@ for(let i = 0; i < scBtnContent.length; i++){
                     trackerThree.classList.add("current")
                     
                     custInfo.addEventListener("click", function(){
+                    let nameDisplay = document.querySelector(".name-field").value;
+                    let surnameDisplay = document.querySelector(".surname-field").value;
+                    let emailDisplay = document.querySelector(".email-field").value;
+                    let returnDisplay = document.querySelector(".returning").value;
+
+                    phaseThree.classList.add("hidden")
+                    phaseFour.classList.remove("hidden")
+
+                    trackerThree.classList.remove("current")
+                    trackerFour.classList.add("current")
+
                         console.log(`you have selected ${currentBtn} `)
                         console.log(getDate)
                         console.log(getTime)
-                        console.log(getName)
-                        console.log(getName)
-                        console.log(getName)
-                        console.log(getName)
+                        console.log(nameDisplay)
+                        console.log(surnameDisplay)
+                        console.log(emailDisplay)
+                        console.log(returnDisplay[0])
                     })
 
                    
