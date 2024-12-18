7fbba3406ccf47521c94cbd5970113a8733e1650
Jose Parra <110458931+JoseParra28@users.noreply.github.com>
2024-12-18 14:17:49 +0000

    js file change

--- gallery.js
+++ gallery.js
@@ -17,125 +17,125 @@ for (i = 0; i < galleryImg.length; i++){
         console.log(this)
         switch (changeImg) {
             case "img-1":
-                image.src = '/assets/image-asset (5).jpeg'
+                image.src = 'assets/image-asset (5).jpeg'
                 addimage[0].appendChild(image)
                 popUp.classList.remove("hidden")
                 break;
 
                 case "img-2":
-                image.src = '/assets/image-asset (2).jpeg'
+                image.src = 'assets/image-asset (2).jpeg'
                 addimage[0].appendChild(image)
                 popUp.classList.remove("hidden")
                 console.log("You clicked 2")
                 break;
 
                             case "img-3":
-                                image.src = '/assets/image-asset (10).jpeg'
+                                image.src = 'assets/image-asset (10).jpeg'
                                 addimage[0].appendChild(image)
                                 popUp.classList.remove("hidden")
                                 console.log("You clicked 2")
                                 break;
 
                                 case "img-4":
-                                    image.src = '/assets/image-asset (3).jpeg'
+                                    image.src = 'assets/image-asset (3).jpeg'
                                     addimage[0].appendChild(image)
                                     popUp.classList.remove("hidden")
                                     console.log("You clicked 2")
                                     break;
 
                                     case "img-5":
-                                        image.src = '/assets/image-asset (4).jpeg'
+                                        image.src = 'assets/image-asset (4).jpeg'
                                         addimage[0].appendChild(image)
                                         popUp.classList.remove("hidden")
                                         console.log("You clicked 2")
                                         break;
 
                                         case "img-6":
-                                            image.src = '/assets/image-asset (6).jpeg'
+                                            image.src = 'assets/image-asset (6).jpeg'
                                             addimage[0].appendChild(image)
                                             popUp.classList.remove("hidden")
                                             console.log("You clicked 2")
                                             break;
 
                                             case "img-7":
-                                                image.src = '/assets/image-asset (8).jpeg'
+                                                image.src = 'assets/image-asset (8).jpeg'
                                                 addimage[0].appendChild(image)
                                                 popUp.classList.remove("hidden")
                                                 console.log("You clicked 2")
                                                 break;
 
                                                 case "img-8":
-                                                    image.src = '/assets/image-asset (7).jpeg'
+                                                    image.src = 'assets/image-asset (7).jpeg'
                                                     addimage[0].appendChild(image)
                                                     popUp.classList.remove("hidden")
                                                     console.log("You clicked 2")
                                                     break;
 
                                                     case "img-9":
-                                                    image.src = '/assets/image-asset (11).jpeg'
+                                                    image.src = 'assets/image-asset (11).jpeg'
                                                     addimage[0].appendChild(image)
                                                     popUp.classList.remove("hidden")
                                                     console.log("You clicked 2")
                                                     break;
 
                                                     case "img-10":
-                                                    image.src = '/assets/image-asset (13).jpeg'
+                                                    image.src = 'assets/image-asset (13).jpeg'
                                                     addimage[0].appendChild(image)
                                                     popUp.classList.remove("hidden")
                                                     console.log("You clicked 2")
                                                     break;
 
                                                     case "img-11":
-                                                    image.src = '/assets/image-asset (9).jpeg'
+                                                    image.src = 'assets/image-asset (9).jpeg'
                                                     addimage[0].appendChild(image)
                                                     popUp.classList.remove("hidden")
                                                     console.log("You clicked 2")
                                                     break;
 
                                                     case "img-12":
-                                                    image.src = '/assets/image-asset (1).jpeg'
+                                                    image.src = 'assets/image-asset (1).jpeg'
                                                     addimage[0].appendChild(image)
                                                     popUp.classList.remove("hidden")
                                                     console.log("You clicked 2")
                                                     break;
 
                                                     case "img-13":
-                                                    image.src = '/assets/image-asset (15).jpeg'
+                                                    image.src = 'assets/image-asset (15).jpeg'
                                                     addimage[0].appendChild(image)
                                                     popUp.classList.remove("hidden")
                                                     console.log("You clicked 2")
                                                     break;
 
                                                     case "img-14":
-                                                    image.src = '/assets/image-asset (17).jpeg'
+                                                    image.src = 'assets/image-asset (17).jpeg'
                                                     addimage[0].appendChild(image)
                                                     popUp.classList.remove("hidden")
                                                     console.log("You clicked 2")
                                                     break;
 
                                                     case "img-15":
-                                                    image.src = '/assets/image-asset (18).jpeg'
+                                                    image.src = 'assets/image-asset (18).jpeg'
                                                     addimage[0].appendChild(image)
                                                     popUp.classList.remove("hidden")
                                                     console.log("You clicked 2")
                                                     break;
 
                                                     case "img-16":
-                                                    image.src = '/assets/image-asset (16).jpeg'
+                                                    image.src = 'assets/image-asset (16).jpeg'
                                                     addimage[0].appendChild(image)
                                                     popUp.classList.remove("hidden")
                                                     console.log("You clicked 2")
                                                     break;
 
                                                     case "img-17":
-                                                        image.src = '/assets/image-asset (12).jpeg'
+                                                        image.src = 'assets/image-asset (12).jpeg'
                                                         addimage[0].appendChild(image)
                                                         popUp.classList.remove("hidden")
                                                         console.log("You clicked 2")
                                                         break;
 
                                                         case "img-18":
-                                                            image.src = '/assets/image-asset (14).jpeg'
+                                                            image.src = 'assets/image-asset (14).jpeg'
                                                             addimage[0].appendChild(image)
                                                             popUp.classList.remove("hidden")
                                                             console.log("You clicked 2")
